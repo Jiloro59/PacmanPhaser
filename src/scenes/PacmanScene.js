44c8681dc2c0a2c4099bd5eade19c94ed769bf62
de sinety Vincent <vdesinety@hotmail.fr>
2022-10-25 15:12:06 +0200

    teleport+ eatPile

--- src/scenes/PacmanScene.js
+++ src/scenes/PacmanScene.js
@@ -7,7 +7,7 @@ export default class PacmanScene extends Phaser.Scene {
   lifes = 3;
   currentDirection = null;
   previousDirection = null;
-  // physics = this.physics;
+  pileCount =0;
 
   preload() {
     this.load.image('tiles', 'assets/images/drawtiles-spaced.png');
@@ -40,7 +40,18 @@ export default class PacmanScene extends Phaser.Scene {
     this.enemyGroup.add(this.ghost4, true);
 
     this.physics.add.overlap(this.player, this.enemyGroup, () => {
-      console.log('OUCH');
+      if(this.lifes >0){
+        this.player.angle = 0;
+        this.player.setVelocity(0, 0);
+        this.player.setPosition(32 +16, 32 +16);
+        this.ghost1.setPosition(256 + 16, 320 + 16);
+        this.ghost2.setPosition(288 + 16, 320 + 16);
+        this.ghost3.setPosition(320 + 16, 320 + 16);
+        this.ghost4.setPosition(288 + 16, 288 + 16);
+        this.previousDirection = null;
+        this.currentDirection = null;
+        this.block.destroy();
+        }
       this.lifes -= 1;
     });
 
@@ -49,6 +60,22 @@ export default class PacmanScene extends Phaser.Scene {
       this.moveDirection();
     }, 100);
   }
+  update() {
+
+    var x = (Math.ceil(this.player.x/32)-1);
+    var y = (Math.ceil(this.player.y/32)-1);
+    if (x >18)x=18
+    if (x <0)x=0
+    
+    var playerPosition = this.map.getTileAt(x, y);
+
+    if(playerPosition.index === 3) { 
+      playerPosition.index = 0;
+        this.pileCount++;
+    // console.log(pileCount);
+    }
+  }
+
   Center(player) {
     if (this.previousDirection === 'up') {
       this.block.destroy();
@@ -92,17 +119,45 @@ export default class PacmanScene extends Phaser.Scene {
       }
     }
   }
-  Collider(player, block) {
-    let collider = this.physics.add.overlap(
-      player,
-      block,
-      function (playerOnBlock) {
-        playerOnBlock.body.stop();
-        this.physics.world.removeCollider(collider);
-      },
-      null,
-      this,
-    );
+  Collider(player, block, tile){
+
+    var tileCollider =tile
+      var collider = this.physics.add.overlap(player, block, function (playerOnBlock){
+
+
+      if (tileCollider.y === 10 && tileCollider.x <= 6){
+        player.x=19*32-16
+        var tile = this.layer.getTileAtWorldXY(16*32 , 10*32, true);
+        console.log(tile)
+
+        var i = 0
+        while(tile.index !== 2 && i <20){
+         tile = this.layer.getTileAtWorldXY(tile.pixelX -32 , tile.pixelY, true);
+         i++;
+        }
+        block = this.physics.add.image(tile.pixelX +16 +5,tile.pixelY +16 , 'tile');
+        console.log(block)
+        block.visible = false;
+          
+        this.Collider(player,block,tile)
+
+        }else if (tileCollider.y === 10 && tileCollider.x >=12){
+           player.x=30
+          tile = this.layer.getTileAtWorldXY(2*32 , 10*32, true);
+          
+          while(tile.index !== 2){
+            tile = this.layer.getTileAtWorldXY(tile.pixelX +32, tile.pixelY, true);
+          }
+          block = this.physics.add.image(tile.pixelX +16 -5,tile.pixelY +16 , 'tile');
+          block.visible = false;
+          
+          this.Collider(player,block,tile)
+      
+        }else{
+          playerOnBlock.body.stop();
+          this.physics.world.removeCollider(collider);
+        }
+      }, null, this);
   }
 
   moveDirection() {
@@ -206,9 +261,9 @@ export default class PacmanScene extends Phaser.Scene {
     let tile = null;
     this.currentDirection = 'left';
     tile = this.layer.getTileAtWorldXY(this.player.x, this.player.y, true);
-    if (tile.y === 10 && tile.x < 6) {
+    if (tile.y === 10 && tile.x <= 6) {
       tile = this.layer.getTileAtWorldXY(16, tile.pixelY, true);
-      tile.pixelX = -32;
+      tile.pixelX = -50;
     } else {
       while (tile.index !== 2) {
         tile = this.layer.getTileAtWorldXY(tile.pixelX - 32, tile.pixelY, true);
@@ -216,6 +271,7 @@ export default class PacmanScene extends Phaser.Scene {
     }
     if (this.player.x - tile.pixelX > 32 + 18) {
       this.Center(this.player);
+      this.player.angle = 180;
       this.block = this.physics.add.image(
         tile.pixelX + 16 + 5,
         tile.pixelY + 16,
@@ -225,7 +281,7 @@ export default class PacmanScene extends Phaser.Scene {
       this.previousDirection = 'left';
       // Move at 100 px/s:
       this.player.setVelocity(-100, 0);
-      this.Collider(this.player, this.block);
+      this.Collider(this.player, this.block,tile);
     }
   }
   moveRight() {
@@ -235,6 +291,7 @@ export default class PacmanScene extends Phaser.Scene {
     tile = this.layer.getTileAtWorldXY(this.player.x, this.player.y, true);
     if (tile.y === 10 && tile.x >= 12) {
       tile = this.layer.getTileAtWorldXY(18 * 32 + 16, tile.pixelY, true);
+      tile.pixelX+= 50;
     } else {
       while (tile.index !== 2) {
         tile = this.layer.getTileAtWorldXY(tile.pixelX + 32, tile.pixelY, true);
@@ -242,6 +299,7 @@ export default class PacmanScene extends Phaser.Scene {
     }
     if (this.player.x - tile.pixelX < -18) {
       this.Center(this.player);
+      this.player.angle = 0;
       this.block = this.physics.add.image(
         tile.pixelX + 16 - 5,
         tile.pixelY + 16,
@@ -250,7 +308,7 @@ export default class PacmanScene extends Phaser.Scene {
       this.block.visible = true;
       this.previousDirection = 'right';
       this.player.setVelocity(+100, 0);
-      this.Collider(this.player, this.block);
+      this.Collider(this.player, this.block,tile);
     }
   }
   moveUp() {
@@ -263,6 +321,7 @@ export default class PacmanScene extends Phaser.Scene {
     }
     if (this.player.y - tile.pixelY > +32 + 18) {
       this.Center(this.player);
+      this.player.angle = 270;
       this.block = this.physics.add.image(
         tile.pixelX + 16,
         tile.pixelY + 16 + 5,
@@ -274,7 +333,7 @@ export default class PacmanScene extends Phaser.Scene {
       // Move at 100 px/s:
       this.player.setVelocity(0, -100);
 
-      this.Collider(this.player, this.block);
+      this.Collider(this.player, this.block,tile);
     }
   }
   moveDown() {
@@ -287,6 +346,7 @@ export default class PacmanScene extends Phaser.Scene {
     }
     if (this.player.y - tile.pixelY < -32 + 14) {
       this.Center(this.player);
+      this.player.angle = 90;
       this.block = this.physics.add.image(
         tile.pixelX + 16,
         tile.pixelY + 16 - 5,
@@ -298,7 +358,7 @@ export default class PacmanScene extends Phaser.Scene {
       // Move at 100 px/s:
       this.player.setVelocity(0, +100);
 
-      this.Collider(this.player, this.block);
+      this.Collider(this.player, this.block,tile);
     }
   }
 }
